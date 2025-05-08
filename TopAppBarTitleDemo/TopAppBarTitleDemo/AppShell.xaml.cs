72d6024a906c57b4b0372187c6642e0ed36d46ec
Timothy Gorman <timothy.gorman@crump.com>
2025-05-08 14:30:14 -0500

    Initial Commit

---
+++ TopAppBarTitleDemo/TopAppBarTitleDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TopAppBarTitleDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TopAppBarTitleDemo/TopAppBarTitleDemo/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TopAppBarTitleDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
